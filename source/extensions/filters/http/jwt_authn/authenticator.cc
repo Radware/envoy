ada44ad6d2393743e0a8ce891199ab8c13b18e64
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-12-21 14:15:37 +0200

    Update authenticator.cc
    Signed-off-by: danieldradware <117576776+danieldradware@users.noreply.github.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -331,7 +331,7 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
   if(Status::Ok != status) {
     //Forward the failed status to dynamic metadata
     ENVOY_LOG(info, "status is: {}",::google::jwt_verify::getStatusString(status));
-    ENVOY_LOG(info, "jwks_data_ is not nullptr? {}",jwks_data_!= nullptr);
+    //ENVOY_LOG(info, "jwks_data_ is not nullptr? {}",jwks_data_!= nullptr);
     //if((jwks_data_ != nullptr) && !jwks_data_->getJwksObj()->keys().empty()){
     //ENVOY_LOG(info,
     //          "### inside if and status in jwks_data is: {}",
@@ -341,15 +341,15 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
     
     
     //if (jwks_data_ != nullptr) {
-      ENVOY_LOG(info, "getJwksObj is empty? {}",jwks_data_->getJwksObj()->keys().empty());
-      ENVOY_LOG(info, "jwks_data_ has value: {]", jwks_data_->getJwksObj()->keys().data()->get()->alg_);
+      //ENVOY_LOG(info, "getJwksObj is empty? {}",jwks_data_->getJwksObj()->keys().empty());
+      //ENVOY_LOG(info, "jwks_data_ has value: {]", jwks_data_->getJwksObj()->keys().data()->get()->alg_);
       if(!jwks_cache_.findByProvider(*provider_)->getJwtProvider().failed_status_in_metadata().empty()) {
         ENVOY_LOG(info,"!jwks_cache_.findByProvider(*provider_)->getJwtProvider().failed_status_in_metadata().empty() == {}",!jwks_cache_.findByProvider(*provider_)->getJwtProvider().failed_status_in_metadata().empty());
       ProtobufWkt::Struct failed_status;
       auto& failed_status_fields = *failed_status.mutable_fields();
       failed_status_fields["status"].set_string_value(std::to_string(enumToInt(status)));
       ENVOY_LOG(debug, "Writing to metada failure reason: {}", google::jwt_verify::getStatusString(status));
-      set_extracted_jwt_data_cb_(jwks_cache_.findByProvider(*provider_)->getJwtProvider().failed_status_in_metadata().c_str(), failed_status);
+      set_extracted_jwt_data_cb_(jwks_cache_.findByProvider(*provider_)->getJwtProvider().failed_status_in_metadata(), failed_status);
     }
   //}
   }
