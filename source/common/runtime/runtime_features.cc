d40d060c220979796e343fbfb5a6906b59ec2e93
danield <danield@radware.com>
2023-08-23 05:20:57 +0000

    changes

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -86,6 +86,7 @@ RUNTIME_GUARD(envoy_reloadable_features_validate_upstream_headers);
 RUNTIME_GUARD(envoy_restart_features_explicit_wildcard_resource);
 RUNTIME_GUARD(envoy_restart_features_remove_runtime_singleton);
 RUNTIME_GUARD(envoy_restart_features_use_apple_api_for_dns_lookups);
+RUNTIME_GUARD(envoy_reloadable_features_token_passed_entirely);
 
 // Begin false flags. These should come with a TODO to flip true.
 // Sentinel and test flag.
